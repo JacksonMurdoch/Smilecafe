33559a33d3e18d40f95a2b128fda9e0aee52b3b0
JacksonMurdoch <Jacksonmurdoch@icloud.com>
2023-02-24 13:55:36 +1300

    HI

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def render_homepage():
+    return render_template("home.html")
+
+@app.route("menu")
+def render_homepage():
+    return render_template("menu.html")
+
+@app.route("contact")
+def render_homepage():
+    return render_template("contact.html")
+
+app.run(host='0.0.0.0', debug=True)
